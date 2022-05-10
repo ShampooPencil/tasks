a01d65ff856f5fd7da1e99679fbd482cf912a015
Andrew Aramouni <aramouni8@gmail.com>
2022-05-10 17:40:20 -0400

    deleted useless pics, folder, files etc

--- task-slicer/src/App.js
+++ task-slicer/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 // **********************************************************
@@ -10,18 +9,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+      
       </header>
     </div>
   );
