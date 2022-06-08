7719aec5c52562520e453c516cb3150a10873bbd
Andrew Aramouni <aramouni8@gmail.com>
2022-06-07 20:07:53 -0400

    Trying onClick on NavBar

--- task-slicer/src/Navbar/NavBar.js
+++ task-slicer/src/Navbar/NavBar.js
@@ -6,28 +6,36 @@ import { Login } from "../LoginOrSignup/Login.js";
 // import ReactDOM from 'react-dom';
 
 export default function NavBar() {
+  let toggleCompDisplay = false;
   function handleTaskSlicerClick() {
-    <TaskSlicer />;
+    toggleCompDisplay = false;
   }
   function handleLoginClick() {
-    <Login />;
+    toggleCompDisplay = true;
   }
+
+  //6/7/2022 8pm
+  //Next time: Try to figure out a system so I can click a NavBar button and
+  //the correct JSX component file will show on screen. I am using the TaskSlicer's
+  // JSX file as a default when site is opened at the beginning
+  //IF I dont have time to actually work on site on pc when free time to think
+  // write some code on paper! or paper talk about on a recorder etc.
 
   //probably add props later on
   const taskSlicerNavBar = (
     <>
       <div className="navContainer">
         <nav>
-          <span onClick={handleTaskSlicerClick}>TaskSlicer</span> |
-          <span onClick={handleLoginClick}>Login</span> |
+          <button onClick={handleTaskSlicerClick}>TaskSlicer</button> |
+          <button onClick={handleLoginClick}>Login</button> |
           <a href="https://www.geeksforgeeks.org/fundamentals-of-algorithms/">
             Contact
           </a>
         </nav>
       </div>
 
-      {/* Defualt component/home */}
-      <TaskSlicer />
+      {/* Default component/home */}
+      {toggleCompDisplay ? <Login /> : <TaskSlicer />}
     </>
   );
 
