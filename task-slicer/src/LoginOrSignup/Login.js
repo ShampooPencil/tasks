ac755ef8fce26138910d2f63e528bb71f8240bd8
Andrew Aramouni <aramouni8@gmail.com>
2022-06-30 07:10:19 -0400

    added login to TaskSlicer component

--- task-slicer/src/LoginOrSignup/Login.js
+++ task-slicer/src/LoginOrSignup/Login.js
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
 export function Login() {
+  const [login, setLogin] = useState("");
+  const [pass_word, setPassWord] = useState("");
+  console.log(login);
   return (
     <>
-      <h2>Login or Sign-Up!</h2>
-      {/* <form>
-        <input>email</input>
-        <input>password</input>
-      </form> */}
+      <div className="container2">
+        <h2>TaskSlicer</h2>
+        <span>email</span>
+        <input
+          className="login"
+          type="text"
+          name="email"
+          placeholder="taskSlicer@gmail.com"
+          value={login}
+          onChange={(event) => setLogin(event.target.value)}
+        />
+        <span>password</span>
+        <input
+          className="password"
+          type="text"
+          name="password"
+          placeholder="**********"
+          value={pass_word}
+          onChange={(event) => setPassWord(event.target.value)}
+        />
+        <br />
+        <span>Forgot Password?</span> {/* add component ForgotPassword */}
+        <span>Privacy</span> {/* add component Privacy */}
+        <span>Sign-up</span>
+      </div>
     </>
   );
 }
--- task-slicer/src/TaskSlicer/TaskSlicer.js
+++ task-slicer/src/TaskSlicer/TaskSlicer.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import SignUp from "../LoginOrSignup/SignUp.js";
+import Login from "../LoginOrSignup/Login.js";
 
 export function TaskSlicer() {
   //First-Goal: this component is going to be the login/log out and signup component
@@ -7,35 +8,10 @@ export function TaskSlicer() {
   const [login, setLogin] = useState("");
   const [pass_word, setPassWord] = useState("");
   console.log(login);
-  console.log(pass_word);
-  function handleSignUpClick() {}
   return (
     <>
-      <div className="container2">
-        <h2>TaskSlicer</h2>
-        <span>email</span>
-        <input
-          className="login"
-          type="text"
-          name="email"
-          placeholder="taskSlicer@gmail.com"
-          value={login}
-          onChange={(event) => setLogin(event.target.value)}
-        />
-        <span>password</span>
-        <input
-          className="password"
-          type="text"
-          name="password"
-          placeholder="**********"
-          value={pass_word}
-          onChange={(event) => setPassWord(event.target.value)}
-        />
-        <br />
-        <span>Forgot Password?</span> {/* add component ForgotPassword */}
-        <span>Privacy</span> {/* add component Privacy */}
-        <span onClick={handleSignUpClick}>Sign-up</span>
-      </div>
+      {/* 1. when first on site/app, login component renders(on TaskSlicer Compoenent)  */}
+      <Login />
     </>
   );
 }
