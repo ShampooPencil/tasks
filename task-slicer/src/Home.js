293bf2ea8f459e0521f31137b5ad330ffd5a38f3
Andrew Aramouni <aramouni8@gmail.com>
2023-02-04 12:14:34 -0500

    using BrowserRouter this time

---
+++ task-slicer/src/Home.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="home-layout">
+      <div>
+        <h1>Task-Slicer</h1>
+        <p>
+          Order your groceries from <em>SuperM</em> with our easy to use app,
+          and get your products delivered straight to your doorstep.
+        </p>
+        <Link to="/taskslicer" className="btn btn-default">
+          add new tasks
+        </Link>
+      </div>
+    </div>
+  );
+}
--- task-slicer/src/NavBar.js
+++ task-slicer/src/NavBar.js
@@ -1,61 +1,23 @@
 import { useState } from "react";
-import { Contact } from "./Contact.js";
-import { TaskSlicer } from "./TaskSlicer.js";
-import { Login } from "./Login.js";
-import { QuickList } from "./QuickList";
-// import "./App.css";
-// import ReactDOM from 'react-dom';
+import { NavLink } from "react-router-dom";
 
-export function NavBar() {
-  const [showDisplay, setDisplay] = useState(<TaskSlicer />);
-
-  function handleTaskSlicerClick() {
-    const showTaskSlicer = <TaskSlicer />;
-    setDisplay(showTaskSlicer);
-  }
-
-  function handleLoginClick() {
-    const showLoginPage = <Login />;
-    setDisplay(showLoginPage);
-    console.log(showLoginPage);
-  }
-  function handleContactClick() {
-    const showContactPage = <Contact />;
-    setDisplay(showContactPage);
-  }
-  function handleQuickListClick() {
-    const showQuickListPage = <QuickList />;
-    setDisplay(showQuickListPage);
-    console.log(showQuickListPage);
-  }
-
-  //6/7/2022 8pm
-  //Next time: Try to figure out a system so I can click a NavBar button and
-  //the correct JSX component file will show on screen. I am using the TaskSlicer's
-  // JSX file as a default when site is opened at the beginning
-  //IF I dont have time to actually work on site on pc when free time to think
-  // write some code on paper! or paper talk about on a recorder etc.
-
-  //probably add props later on
-  function taskSlicerNavBar() {
-    return (
-      <>
-        <div className="navContainer">
-          <nav>
-            ||<button onClick={handleTaskSlicerClick}>TaskSlicer</button> ||
-            <button onClick={handleLoginClick}>Login</button> ||
-            <button onClick={handleContactClick}>Contact</button> ||
-            <button onClick={handleQuickListClick}>QuickList</button>||
-          </nav>
-        </div>
-
-        {/*First: create a card-like Forum for Logging in/Loggin out
-        and also signup under login
-        (maybe draw or whiteboard the generally blueprints of it*/}
-        <div>{showDisplay}</div>
-      </>
-    );
-  }
-
-  return taskSlicerNavBar();
+//probably add props later on
+export default function taskSlicerNavBar() {
+  return (
+    <>
+      <div className="navContainer">
+        <nav>
+          <NavLink to="/" className="navLinks">
+            Home
+          </NavLink>
+          <NavLink to="contact" className="navLinks">
+            Contact
+          </NavLink>
+          <NavLink to="taskslicer" className="navLinks">
+            TaskSlicer
+          </NavLink>
+        </nav>
+      </div>
+    </>
+  );
 }
