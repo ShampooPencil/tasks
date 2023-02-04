aceab7bfdc38d830960f12d3c17cc263b10c0a23
Andrew Aramouni <aramouni8@gmail.com>
2023-02-04 13:00:08 -0500

    using BrowserRouter this time

--- task-slicer/src/index.js
+++ task-slicer/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./index.html";
 import "./index.css";
 // import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
