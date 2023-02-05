b2e5b501e58dee237246506a093d3bfa3a77b683
Andrew Allen Aramouni <aramouni8@gmail.com>
2023-02-05 14:41:07 -0500

    onClick on adding task btn

--- task-slicer/src/TaskSlicer.js
+++ task-slicer/src/TaskSlicer.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 
 export default function TaskSlicer() {
+  const [task, setTasks] = useState([])
   // 2/4/23UPDATE: just work on the app itself and don't worry about logins or signups right nowFirst-Goal: this component is going to be the login/log out and signup component
   // (add those components later on this component)
   // const [login, setLogin] = useState("");
   // const [pass_word, setPassWord] = useState("");
   // console.log(login);
+  const addTaskInput = <input type="text"></input>
+  function openPrompt() {
+    return addTaskInput
+  }
   function clickNewTaskForm() {
     // for clicking on new task button
+    openPrompt()
+    setTasks([...task, addTaskInput]);
+    console.log(task);
   }
   function handleAddNewTask() {
     // add new task to state
@@ -18,7 +26,7 @@ export default function TaskSlicer() {
       <div className="card">
         {/* 1. click on new tasks
             -create a function so the user can type its new task */}
-        <button>+ New Task</button>
+        <button onClick={() => clickNewTaskForm}>+ New Task</button>
         {/* 2. create handle function so when click on add the task, it will store it to task state
              -test if new task is added to task state
              -might have to create a new state so task is immutable */}
