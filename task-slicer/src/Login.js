2f089313e010496774f847bd0c47ac7accdc7b04
Andrew Aramouni <aramouni8@gmail.com>
2023-03-06 15:55:32 -0500

    starting to work on login page

--- task-slicer/src/Login.js
+++ task-slicer/src/Login.js
@@ -1,14 +1,35 @@
+import { useState } from "react";
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [dataInput, setDataInput] = useState([]);
+
+  const submitThis = () => {
+    const info = { email: email, password: password };
+    setDataInput([info]);
+  };
   return (
     <div>
-      <form action="">
+      <form action="" onSubmit={submitThis}>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" />
+          <input
+            type="text"
+            name="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="password">Password</label>
-          <input type="text" name="passw" id="passw" />
+          <input
+            type="text"
+            name="passw"
+            id="passw"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <button type="submit">Login</button>
       </form>
